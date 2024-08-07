5e2c50556ba1d4145a9af16f1e2c638201bfd098
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2024-08-07 07:46:28 -0400

    build: clean up `build.py` a bit

--- build.py
+++ build.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import os
 import shutil
+import sys
 from pathlib import Path
 
 # setuptools *must* come before Cython, otherwise Cython's distutils hacking
@@ -15,38 +16,25 @@ SOURCE_DIR = Path("koerce")
 BUILD_DIR = Path("cython_build")
 
 
+extensions = [
+    Extension("koerce.builders", ["koerce/builders.py"]),
+    Extension("koerce.patterns", ["koerce/patterns.py"]),
+    Extension("koerce.utils", ["koerce/utils.py"]),
+]
+
 cythonized_modules = cythonize(
-    [
-        Extension(
-            "koerce.builders",
-            ["koerce/builders.py"],
-        ),
-        Extension(
-            "koerce.patterns",
-            ["koerce/patterns.py"],
-        ),
-        Extension(
-            "koerce.utils",
-            ["koerce/utils.py"],
-        ),
-    ],
+    extensions,
     build_dir=BUILD_DIR,
     # generate anannotated .html output files.
     annotate=True,
-    # nthreads=multiprocessing.cpu_count() * 2,
+    nthreads=min(os.cpu_count(), len(extensions)),
     compiler_directives={
         "language_level": "3",
         "binding": False,
         "boundscheck": False,
         "nonecheck": False,
         "always_allow_keywords": False,
-        # "wraparound": False,
-        # "profile": True,
-        # "annotation_typing": False
     },
-    # always rebuild, even if files untouched
-    force=False,
-    # emit_linenums=True
 )
 
 dist = Distribution({"ext_modules": cythonized_modules})
@@ -57,10 +45,10 @@ cmd.run()
 # remove all binaries *.so, *.dll, *.lib, *.pyd, *.dylib
 for ext in ["so", "dll", "lib", "pyd", "dylib"]:
     for f in SOURCE_DIR.glob(f"*.{ext}"):
-        print(f"Removing previously built binary {f}")  # noqa: T201
         f.unlink()
+        print(f"removed {f!r}", file=sys.stderr)  # noqa: T201
 
 for output in cmd.get_outputs():
     relative_extension = os.path.relpath(output, cmd.build_lib)
-    print(f"Copying {output} to {relative_extension}")  # noqa: T201
+    print(f"{output} -> {relative_extension}", file=sys.stderr)  # noqa: T201
     shutil.copyfile(output, relative_extension)
